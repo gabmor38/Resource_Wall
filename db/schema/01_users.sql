b3c16ed2689f1761c1db8f3e2dd354787c4d1573
gabmor38 <Gabriela.morenor@gmail.co>
2021-10-21 22:48:24 -0400

    seeds and schema creation

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,37 @@
 -- Drop and recreate Users table (Example)
-
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS resources CASCADE;
+DROP TABLE IF EXISTS reviews CASCADE;
+DROP TABLE IF EXISTS saved CASCADE;
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE resources (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  number_of_likes INTEGER,
+  url VARCHAR(255) NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description VARCHAR(255) NOT NULL,
+  category VARCHAR(255) NOT NULL,
+  date date NOT NULL
+);
+
+CREATE TABLE reviews (
+  id SERIAL PRIMARY KEY NOT NULL,
+  resource_id INTEGER REFERENCES resources(id) ON DELETE CASCADE,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  rating SMALLINT NOT NULL DEFAULT 0,
+  message TEXT,
+  date date NOT NULL
+);
+
+CREATE TABLE saved (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  resource_id INTEGER REFERENCES resources(id) ON DELETE CASCADE
 );
--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,15 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (email, password) VALUES ('Alice@gmail.com', 'password123');
+INSERT INTO users (email, password) VALUES ('Kira@yahoo.com','password12');
+INSERT INTO users (email, password) VALUES ('Leila@hotmail.com', 'password');
+
+INSERT INTO resources (number_of_likes, url, title, description, category, date) VALUES (3, 'https://jquery.com', 'Jquery', 'Jquery documentation', 'Computer Science','2021-09-10');
+INSERT INTO resources (number_of_likes, url, title, description, category, date) VALUES (2,'https://www.khanacademy.org/math', 'Math', 'Math article and exercises','Math', '2020-01-01');
+INSERT INTO resources (number_of_likes, url, title, description, category, date) VALUES (1,'https://brainyball.blogspot.com','Learn Math','Math blog','Math','2021-02-05');
+INSERT INTO resources (number_of_likes, url, title, description, category, date) VALUES (4,'https://science4fun.info','Learn Science','Informative article','Science','2021-02-05');
+
+INSERT INTO reviews (rating, message, date) VALUES ('4','Great documentation','2021-09-12');
+INSERT INTO reviews (rating, message, date) VALUES ('4','Very informative','2020-06-03');
+INSERT INTO reviews (rating, message, date) VALUES ('2','Needs more information','2021-06-03');
+INSERT INTO reviews (rating, message, date) VALUES ('3','Fun article about science','2021-06-03');
+
