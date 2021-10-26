8de669d7d56d3e0a19b832565800de5b22073279
Harjeet Kaur <harjeetkaur.dhanjal@gmail.com>
2021-10-26 17:00:26 -0400

    fixed register,login and logout

--- server.js
+++ server.js
@@ -7,10 +7,7 @@ const sassMiddleware = require("./lib/sass-middleware");
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
-const bcrypt = require('bcrypt');
-const saltrounds = 10;
 
 // PG database client/connection setup
 const { Pool } = require("pg");
@@ -22,7 +19,6 @@ app.use(cookieSession({
   keys: ['key1', 'key2']
 }));
 
-
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
@@ -30,8 +26,6 @@ app.use(morgan("dev"));
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 app.use(
   "/styles",
@@ -47,14 +41,12 @@ app.use(express.static("public"));
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
-const resourcesRoutes = require("./routes/resources");
-const apiRoutes = require("./routes/apiRoutes");
+const widgetsRoutes = require("./routes/widgets");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
-app.use("/api/resources", resourcesRoutes(db));
-app.use("/api", apiRoutes(db));
+app.use("/api/widgets", widgetsRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
