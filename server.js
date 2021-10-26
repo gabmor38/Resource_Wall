e27b7568489a444eaea63b51a8d5a20a204d787a
gabmor38 <Gabriela.morenor@gmail.co>
2021-10-25 22:37:00 -0400

    modify partial header

--- server.js
+++ server.js
@@ -47,13 +47,13 @@ app.use(express.static("public"));
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
-const resourcesRoutes = require("./routes/resources");
+//const resourcesRoutes = require("./routes/resourcesRoutes");
 const apiRoutes = require("./routes/apiRoutes");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
-app.use("/api/resources", resourcesRoutes(db));
+//app.use("/api/resources", resourcesRoutes(db));
 app.use("/api", apiRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
