5e24428bba3356e8c37cc7f1af6ca501a602f934
gabmor38 <Gabriela.morenor@gmail.co>
2021-10-27 19:58:27 -0400

    seed add for user_ids in reviews table

--- db/seeds/DB_seeds.sql
+++ db/seeds/DB_seeds.sql
@@ -9,7 +9,7 @@ INSERT INTO resources (user_id, number_of_likes, url, title, description, catego
 INSERT INTO resources (user_id, number_of_likes, url, title, description, category, date) VALUES (1, 4,'https://science4fun.info','Learn Science','Informative article','science','2021-02-05');
 
 INSERT INTO reviews (resource_id, user_id, rating, message, date) VALUES (1, 1, 4,'Great documentation','2021-09-12');
-INSERT INTO reviews (resource_id, user_id, rating, message, date) VALUES (2, 4, 4,'Very informative','2020-06-03');
+INSERT INTO reviews (resource_id, user_id, rating, message, date) VALUES (2, 1, 4,'Very informative','2020-06-03');
 INSERT INTO reviews (resource_id, user_id, rating, message, date) VALUES (3, 1, 2,'Needs more information','2021-06-03');
-INSERT INTO reviews (resource_id, user_id, rating, message, date) VALUES (4, 3, 1'Fun article about science','2021-06-03');
+INSERT INTO reviews (resource_id, user_id, rating, message, date) VALUES (4, 1, 1,'Fun article about science','2021-06-03');
 
