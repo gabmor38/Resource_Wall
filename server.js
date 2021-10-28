1a239c4843a4fe638de59b6c1e1d0fc201d43ac5
gabmor38 <Gabriela.morenor@gmail.co>
2021-10-27 20:00:30 -0400

    require new_resource

--- server.js
+++ server.js
@@ -42,11 +42,13 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+const userResources = require("./routes/new_resource");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+app.use('/api/new_resource',userResources(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
