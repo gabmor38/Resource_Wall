4413582cb21bce0596a0018cd3c99712e6dca029
gabmor38 <Gabriela.morenor@gmail.co>
2021-10-28 14:50:53 -0400

    fix rerouting

--- routes/new_resource.js
+++ routes/new_resource.js
@@ -3,20 +3,10 @@ const router = express.Router();
 
 module.exports = (db) => {
 
-  // router.get("/", (req, res) => {
-  //   const query = `SELECT * FROM resources`;
-  //   db.query(query)
-  //     .then((data) => {
-  //       res.json(data.rows)
-  //     })
-  //     .catch((e) => {
-  //       res.status(500).json ({error: e.message});
-  //   });
 
   router.get('/new', (req, res) => {
     const user_id = req.session.user.id;
     const templateVars = { user: user_id };
-    console.log("this is the vars in New Get",templateVars);
     if (!user_id) {
       res.redirect('/', templateVars);
     } else {
@@ -27,11 +17,11 @@ module.exports = (db) => {
     }
   });
 
+// this route creates a new card//
   router.post('/new', (req, res) => {
     const user_Id = req.session.user.id;
     const { title, url, description, category } = req.body;
     const date = new Date();
-    console.log("this is the reqbody",req.body);
     console.log(req.session);
     db.query(
       `INSERT INTO resources (user_Id, url, title, description, category, date) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`,
@@ -39,12 +29,10 @@ module.exports = (db) => {
     )
       .then((data) => {
         const newResource = data.rows[0];
-        console.log("thisis the data",newResource);
-        res.redirect('/', newResource);
+        res.redirect('/');
       })
       .catch((error) => {
-        console.log(error)
-        // res.status(500).json({ error: error.message });
+        res.status(500).json({ error: error.message });
       });
   });
 
