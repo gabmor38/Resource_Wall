f287cc7fd14d9321ae36bb20a121de99b4b36243
gabmor38 <Gabriela.morenor@gmail.co>
2021-10-29 14:55:48 -0400

    delete comments

--- routes/new_comment.js
+++ routes/new_comment.js
@@ -3,54 +3,27 @@ const router = express.Router();
 
 module.exports = (db) => {
   router.get('/:id', (req, res) => {
-    console.log("is", req.params.id);
-
-    const templateVars = { user: req.session.user };
-    const user = req.session.user.id;
+    // const templateVars = { user: req.session.user };
+    // const user = req.session.user.id;
     const resource_id = req.params.id;
     console.log("Req sess user", req.session.user);
     console.log("Params", resource_id);
     if (req.session.user) {
       const user = req.session.user.id;
       console.log(`user: ${user}`);
-      // const query = (`SELECT resources.id AS resource_id,
-      // resources.title,
-      // resources.url,
-      // resources.description,
-      // users.id AS User_id
-      // FROM resources JOIN users ON resources.user_id = users.id
-      // WHERE users.id = $1
-      // GROUP BY resources.id,
-      // resources.title,
-      // resources.url,
-      // resources.description,
-      // resources.user_id,
-      // users.id;`);
-
-      // const values = [user];
-      // db.query(query, values)
-      // .then(result => {
-
       const templateVars = {
         resource_id: req.params.id,
         user: user
       };
-      // console.log("rows", result.rows[0])
       res.render('new_comment', templateVars);
-
     }
   });
-
-
+//Adds a comment and redirects to the main resource page.
   router.post('/comment', (req, res) => {
     console.log(req.body);
     const user = req.session.user.id;
-    // console.log(res.rows.id);
     const resource_Id = req.body.resource_id;
     const message = req.body.message;
-    // console.log("This is the user_id",req.session.user.id);
-    // console.log("resourceId", resource_Id);
-
     db.query(
       `INSERT INTO comments (user_Id, resource_id, message) VALUES ($1, $2, $3) RETURNING *`,
       [user, resource_Id, message]
@@ -59,17 +32,12 @@ module.exports = (db) => {
         // templateVars={
         //   ResourceId : data.rows,
         //   user : req.session.user.id
-
         // }
-        // res.redirect("/api/resources/:id");
-        res.send('success')
+        res.redirect("/api/resources/");
       })
       .catch((error) => {
         res.status(500).json({ error: error.message });
       });
   });
-
-
   return router;
-
 };
